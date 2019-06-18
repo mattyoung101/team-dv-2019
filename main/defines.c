377124de8fe5e54ca4fbc72e92fef931263b2368
Matt Young <SodiumSandwich@outlook.com>
2019-06-18 15:58:14 +1000

    NVS TSOP calibration is confirmed working

--- main/defines.c
+++ main/defines.c
@@ -10,12 +10,13 @@ void defines_init(uint8_t robotId){
     if (robotId == 0){
         float values[] = {1.0f, 0.8f, 0.6f, 0.6f, 0.6f, 0.5f, 1.0f, 1.0f, 0.2f, 0.2f, 1.2f, 1.2f, 1.0f, 1.0f, 1.0f, 1.0f, 
                         1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-        // fuckin C bullshit workaround
+        // fuckin C bullshit workaround to set values
         memcpy(TSOP_TUNING, values, TSOP_NUM * sizeof(float));
     } else {
         float values[] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
                         1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-        // fuckin C bullshit workaround
         memcpy(TSOP_TUNING, values, TSOP_NUM * sizeof(float));
     }
+
+    ESP_LOGD(TAG, "Just making sure it works: %f,%f,%f,%f", TSOP_TUNING[0], TSOP_TUNING[1], TSOP_TUNING[2], TSOP_TUNING[3]);
 }
