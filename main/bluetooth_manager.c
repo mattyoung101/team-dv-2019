716ff4af2d18c9b995fc1eb228732e11e624f8c8
Matt Young <SodiumSandwich@outlook.com>
2019-06-23 14:14:10 +1000

    Re-implemented switching and conflict resolution for BT

--- main/bluetooth_manager.c
+++ main/bluetooth_manager.c
@@ -4,7 +4,7 @@
 
 static dv_timer_t packetTimer = {NULL, false};
 static dv_timer_t cooldownTimer = {NULL, false};
-static SemaphoreHandle_t cooldownSem = NULL;
+static bool cooldownOn = false; // true if the cooldown timer is currently activated
 
 static void packet_timer_callback(TimerHandle_t timer){
     static const char *TAG = "BTTimeout";
@@ -24,7 +24,7 @@ static void packet_timer_callback(TimerHandle_t timer){
 static void cooldown_timer_callback(TimerHandle_t timer){
     static const char *TAG = "CooldownTimer";
     ESP_LOGI(TAG, "Cooldown timer gone off, re-enabling switch");
-    xSemaphoreGive(cooldownSem);
+    cooldownOn = false;
     dv_timer_stop(&cooldownTimer);
 }
 
@@ -38,7 +38,6 @@ void comms_bt_receive_task(void *pvParameter){
     // create timers and semaphore if they've not already been created in a previous run of this task
     dv_timer_check_create(&packetTimer, "BTTimeout", BT_PACKET_TIMEOUT, pvParameter, packet_timer_callback);
     dv_timer_check_create(&cooldownTimer, "CooldownTimer", BT_SWITCH_COOLDOWN, pvParameter, cooldown_timer_callback);
-    if (cooldownSem == NULL) cooldownSem = xSemaphoreCreateBinary();
 
     ESP_LOGI(TAG, "Bluetooth receive task init OK, handle: %d", handle);
     esp_task_wdt_add(NULL);
@@ -59,12 +58,23 @@ void comms_bt_receive_task(void *pvParameter){
         if ((isAttack && robotState.outIsAttack) || (!isAttack && !robotState.outIsAttack)) {
             ESP_LOGW(TAG, "Conflict detected: I'm %s, other is %s", robotState.outIsAttack ? "ATTACK" : "DEFENCE",
             isAttack ? "ATTACK" : "DEFENCE");
+            ESP_LOGD(TAG, "my goal dist: %d, other goal dist: %f", robotState.inGoalLength, recvMsg.goalLength);
 
-            if (recvMsg.goalLength < robotState.inGoalLength){
-                ESP_LOGI(TAG, "Conflict resolution: other robot is closest to goal, do nothing");
-            } else {
-                ESP_LOGI(TAG, "Conflict resolution: I'm closest to goal, switch to defence");
+            if (recvMsg.goalLength <= 0.01f){
+                ESP_LOGI(TAG, "Conflict resolution: other robot cannot see goal, I will become defender");
                 fsm_change_state(stateMachine, &stateDefenceDefend);
+            } else if (!robotState.inGoalVisible){
+                ESP_LOGI(TAG, "Conflict resolution: I cannot see goal, becoming attacker");
+                fsm_change_state(stateMachine, &stateAttackPursue);
+            } else {
+                // both robots can see the goal (or neither robot can see the goal, but that shouldn't happen)
+                if (recvMsg.goalLength < robotState.inGoalLength){
+                    ESP_LOGI(TAG, "Conflict resolution: other robot is closest to goal, switch to attack");
+                    fsm_change_state(stateMachine, &stateAttackPursue);
+                } else {
+                    ESP_LOGI(TAG, "Conflict resolution: I'm closest to goal, switch to defence");
+                    fsm_change_state(stateMachine, &stateDefenceDefend);
+                }
             }
         }
 
@@ -76,16 +86,28 @@ void comms_bt_receive_task(void *pvParameter){
             }
             wasSwitchOk = true;
 
-            // if we're OK to switch and we're not in the cooldown period, switch
-            if (robotState.outSwitchOk && xSemaphoreTake(cooldownSem, 0)){
+            // if we're OK to switch and we're not in the cooldown period and we're the switch listener, switch
+            // only one robot (robot 0) will be able to broadcast switch statements to save them both from
+            // switching at the same time
+            if (robotState.outSwitchOk && !cooldownOn && robotState.inRobotId == 0){
                 ESP_LOGI(TAG, "I'm also willing to switch: switching NOW!");
                 esp_spp_write(handle, 6, switchBuffer);
-                fsm_change_state(stateMachine, &stateDefenceDefend); 
+                
+                // invert state
+                if (robotState.outIsAttack){
+                    fsm_change_state(stateMachine, &stateDefenceDefend); 
+                } else {
+                    fsm_change_state(stateMachine, &stateAttackPursue);
+                }
+
+                // start cooldown timer
+                cooldownOn = true;
                 xTimerStart(cooldownTimer.timer, portMAX_DELAY);
                 alreadyPrinted = false;
             }
         } else if (wasSwitchOk){
             // if the other robot is not willing to switch, but was previously willing to switch
+            // TODO it spam prints here when it shouldn't
             ESP_LOGW(TAG, "Other robot is NO LONGER willing to switch");
             wasSwitchOk = false;
             alreadyPrinted = false;
